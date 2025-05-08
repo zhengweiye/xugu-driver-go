eaa0b92fbb2420f3288816f810652222990bc3f0
郑伟业 <286128576@qq.com>
2025-05-08 17:22:19 +0800

    调试

--- test/XuguTest.go
+++ test/XuguTest.go
@@ -6,6 +6,8 @@ import (
 	_ "github.com/zhengweiye/xugu-driver-go"
 )
 
+// linux：/usr/lib64/libxugusql.so
+// windows：C:\Windows\System32\xugusql.dll
 func main() {
 	dbObj, err := sql.Open("xugusql", "IP=192.168.0.239;DB=brain;User=brain_sys;PWD=brain_sys@123;Port=5138;AUTO_COMMIT=on;CHAR_SET=UTF8")
 	if err != nil {
