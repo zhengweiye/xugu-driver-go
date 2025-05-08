20792042fbba4798512b06d8cf47ae43e75be7ce
郑伟业 <286128576@qq.com>
2025-05-08 17:06:03 +0800

    调试

--- test/XuguTest.go
+++ test/XuguTest.go
@@ -7,12 +7,12 @@ import (
 )
 
 func main() {
-	dbObj, err := sql.Open("xugusql", "IP=10.254.47.239;DB=brain;User=brain_sys;PWD=brain_sys@123;Port=5138;AUTO_COMMIT=on;CHAR_SET=UTF8")
+	dbObj, err := sql.Open("xugusql", "IP=192.168.0.239;DB=brain;User=brain_sys;PWD=brain_sys@123;Port=5138;AUTO_COMMIT=on;CHAR_SET=UTF8")
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf(">>>>>>>>>>>>>>>>>>>>>>>>%+v\n", dbObj)
-	rows, err := dbObj.Query("select * from sys_user", []any{}...)
+	rows, err := dbObj.Query("select * from sys_role", []any{}...)
 	if err != nil {
 		panic(err)
 	}
